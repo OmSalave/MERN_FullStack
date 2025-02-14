1c2be9680352275169720b478b6158412b45d77c
Om_Salave <145266959+OmSalave@users.noreply.github.com>
2025-02-14 23:52:17 +0530

    Add files via upload

---
+++ javaScript/Beginner/string.js
@@ -0,0 +1,45 @@
+
+// String Methods
+
+//  trim
+
+let name = prompt("enter your name");
+let newName = name.trim();
+console.log(newName);
+
+// lower case and upper case
+
+console.log(newName.toUpperCase());
+console.log(newName.toLowerCase());
+
+// sring methods with arguments 
+//  index of 
+
+console.log(newName.indexOf ("k"));
+
+// method chaning
+
+console.log(name.trim().toUpperCase());
+
+// slice
+
+let msg = "Ilovecoding"
+
+console.log(msg.slice(4))
+
+// replace
+
+console.log(msg.replace("love","respect"));
+console.log(msg.replace("l","h"));
+
+// question 
+
+let x = "ApnaCollege";
+
+console.log(x.slice(-7).replace("l","t").replace("l","t"));
+
+
+// ARRAYS
+
+
+
