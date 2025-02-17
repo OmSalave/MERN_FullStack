0a06b1fd05088fc15ce87e300c77bf66c03407ed
Om_Salave <145266959+OmSalave@users.noreply.github.com>
2025-02-17 23:51:03 +0530

    Add files via upload

---
+++ javaScript/Beginner/lec7.js
@@ -0,0 +1,62 @@
+// const student = {
+//     name: "om",
+//     math: 98,
+//     phy: 92,
+//     eng: 94,
+
+//     getAvg() {
+//         let avg = (this.math + this.phy + this.eng)/3 ;
+//         console.log(avg)
+// }
+// }
+
+// try and catch 
+
+/// arrow function 
+
+// const avg = (a,b) => {
+//     console.log((a+b)/2);
+// }
+
+// arrow function implicite return 
+
+// const func = (a,b) => (     //  () brackets used
+//     a+b
+// )
+
+//// set timeout 
+
+// console.log("hi");
+
+// setTimeout( () => {
+//         console.log("hello");
+//     },
+
+//     4000);
+
+// console.log("namaste")
+
+/// set interval
+
+// setInterval ( ()=> {
+//     console.log("msg after 2 s")
+// }, 1000);
+
+
+// questions 1
+
+// const square = n => {   /// no () req when single argument 
+//     return(n*n);
+// }
+
+// 2
+
+// setTimeout(()=> {
+//     clearInterval(id)
+// } ,10001);
+
+// let id = setInterval( ()=> {
+//     console.log("Hello World")
+// }, 2000);
+
+
